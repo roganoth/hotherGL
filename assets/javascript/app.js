e9f1cf7e8ff0902f2eb587fb8aa2a0280a878965
dan demeo <chronos937@gmail.com>
2019-10-15 17:00:37 -0400

    added some images to the folder, created divs for quiz area, began to code quiz

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+var correct = 0;
+var incorrect = 0;
+var questions = []
+
+function start () {
+    var chosenQuestion = Math.floor(Math.random)*questions.length;
+    
+}
+
+$("#start").click(function){
+    start();
+}
+})
