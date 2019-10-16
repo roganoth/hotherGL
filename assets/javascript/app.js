37c478bcbd1422c334a5c12f41ad556beff5d174
dan <chronos937@gmail.com>
2019-10-15 20:16:44 -0400

    added comments to help guide my code later

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 var correct = 0;
 var incorrect = 0;
 var questions = []
-
+//create an array of objects(questions), with an array ie question: -> keyAnswers [] -> rightAnswer[] 
 function start () {
     var chosenQuestion = Math.floor(Math.random)*questions.length;
     
